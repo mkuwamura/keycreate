c4395675d4e0597fd803ce2987346d38079d2445
mkuwamura <mk2row@yahoo.co.jp>
2014-06-21 23:25:06 +0900

    Update key_create.sh

--- key_create.sh
+++ key_create.sh
@@ -4,8 +4,8 @@ PUB="ssh-rsa ABAAB3NzaC1yc2EAAAABIwAAAQEA0ODytizWnCRC8A7UXTH9o45eewj1KpAl4HFdjCS
 
 ### ここからkeyが既に作成済みの場合
 
-pub_create () {
-	mkdir ~/.ssh
+pubc () {
+		mkdir ~/.ssh
         cat << EOF > $AUTHFILE
 $PUB
 EOF
@@ -24,46 +24,39 @@ mkdir_ssh () {
 
 
 ### ここからスクリプト削除
-me_delete(){
+me_delete (){
         rm -i $0
 }
 #### ここまでスクリプト削除
 
-key_create(){
+keyc (){
         ssh-keygen  -t rsa -N "" -f ~/.ssh/`whoami`
         mv ~/.ssh/`whoami`.pub $AUTHFILE
         chmod 600 $AUTHFILE
         echo "### Copy the following ###"
         cat ~/.ssh/`whoami`
         echo "##########################"
-
-        rm  ~/.ssh/`whoami`
-        echo "### KEY was DELETED ###"
 }
 
 ### 実際の稼働スクリプト
 
-
+#key_add (){
 if [ ! -d ~/.ssh ]; then
         echo "### key create now. ####"
-        key_create
+        $1
 #       pub_create
-	else
-		if [ -f  ~/.ssh/`whoami` ]
+        elif [ -f  ~/.ssh/`whoami`.pub ]; then
 
-	echo "### ~/.ssh/`whoami` was present. ####"
-
-	 else 
-	      key_create
-		  exit
-		 fi
-				
-		if [ -f $AUTHFILE ];then
-                        echo "###" $AUTHFILE "was present."
+        echo "### ~/.ssh/`whoami`.pub was exist. ####"
+ 		elif [ -f $AUTHFILE ]; then
+                        echo "###" $AUTHFILE "was exist."
                         cat $AUTHFILE
+                        exit
         else
-                key_create
-                #pub_create
+		$1
 
-        fi
+ 
+fi
+ exit 0
+#
 #me_delete
